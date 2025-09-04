9726b3902cbe3da5e73eb264d33c64ebb32fb1d3
Tanvi <tanvitater8@gmail.com>
2025-09-04 08:44:08 +0530

    Conditionalas :if, if-else,if-else laddar

--- JS03/index.js
+++ JS03/index.js
@@ -1,7 +1,7 @@
 console.log("Hello , today we are going to learn about if else statements in JavaScript");
 console.log("\n");
-let age = 17;
-let grace  = 2;
+let age = 0;
+let grace  = 0;
 age += grace;
 console.log("Your age is " + age);
 if((age + grace) > 18){
@@ -10,9 +10,50 @@ if((age + grace) > 18){
 else{
     console.log("you cannot drive");
 }
-console.log(age + grace);
-console.log(age - grace);
-console.log(age * grace);
-console.log(age / grace);
-console.log(age ** grace);
-console.log(age % grace);
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+if("3" === 3){  // checks the value as well as the type
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
+
+if("3" == 3){  // checks only the value
+    console.log("this is true");
+}   
+// if("3" !== 3){  // checks the value as well as the type
+//     console.log("this is true");
+// }   
+// else{
+//     console.log("this is false");
+// }
+
+if(age == 18){
+    console.log("You can drive");
+}
+else if(age == 0){
+    console.log("You are a baby");
+}
+else{
+    console.log("You cannot drive");
+}
+let a = 6;
+let b = 8;
+let c = a>b ?(a+b):(b-a);
+console.log(c);
+
+/*
+translates to 
+if(a>b){
+    c = a + b;
+}
+else{
+    c = b - a;
+}
+*/
