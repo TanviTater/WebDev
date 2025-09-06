b468fe70476cfdc96114b6b918eb456b7aa41a51
Tanvi <tanvitater8@gmail.com>
2025-09-06 09:09:39 +0530

    Business Name Generator

--- Exercise9/index.js
+++ Exercise9/index.js
@@ -4,17 +4,31 @@ let rand  = Math.random();
 let first,second,third;
 if(rand < 0.33){
     first = "Crazy";
-    second = "Engine"
-    third = "Bros"
 }
 else if(rand>=0.33 && rand<0.66){
     first = "Amazing";
-    second = "Foods"
-    third = "Limited"
 }
 else{
     first = "Fire";
+}
+rand = Math.random();
+if(rand < 0.33){
+    second = "Engine"
+}
+else if(rand>=0.33 && rand<0.66){
+    second = "Foods"
+}
+else{
     second = "Garments"
+}
+rand = Math.random();
+if(rand < 0.33){
+    third = "Bros"
+}
+else if(rand>=0.33 && rand<0.66){
+    third = "Limited"
+}
+else{
     third = "Hub"
 }
 
