934aff7d789a16add781a0c7f118fbbe2a423661
Tanvi <tanvitater8@gmail.com>
2025-09-10 09:58:04 +0530

    Factorail Calculation

--- Exercise10/index.js
+++ Exercise10/index.js
@@ -4,4 +4,17 @@ let fact = 1;
 for(let i =1;i<=a;i++){
     fact = fact*i;
 }
+console.log("Method 1 ->")
 console.log(fact);
+
+console.log("Method 2 ->")
+let b = 6;
+function factorial(number){
+    let arr = Array.from(Array(number+1).keys());
+    console.log(arr.slice(1,));
+    let c = arr.slice(1,).reduce((a,b) =>{
+        return a*b;
+    })
+    console.log(c);
+}
+factorial(b);
